3d82ed7b1d3fd9631b2e80ae96847a10fbc9daab
Inma <ireyes@paradigmadigital.com>
2020-11-22 15:31:45 +0100

    basic styles + grid list of gifs

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -2,10 +2,11 @@ import { Link } from "wouter";
 import 'components/Gif/styles.scss'
 
 export default function Gif ({title, id, url}) {
-  return(
-    <Link className='Gif' to={`/gif/${id}`}>
-      <h6>{title}</h6>
-      <img alt={title} src={url} />
-    </Link> 
+  return(    
+    <div className="Gif">
+      <Link to={`/gif/${id}`}>
+        <img className='Gif__img' alt={title} src={url} />
+      </Link> 
+    </div>
   )
 }
