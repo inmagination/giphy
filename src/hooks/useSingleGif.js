e067656e291af317797f7eacd25b61f7025f7259
Inma <ireyes@paradigmadigital.com>
2020-12-01 12:18:08 +0100

    Pagina de detalles retoques (spinner y 404) + loading spinner y redirección a página de error + control en hook de gif unico y usado en pagina de detalles

--- src/hooks/useSingleGif.js
+++ src/hooks/useSingleGif.js
@@ -5,15 +5,27 @@ import getSingleGif from 'services/getSingleGif';
 export default function useSingleGif ({ id }) {
   const {gifs} = useGifs()
   const gifFromCache = gifs.find(singleGif => singleGif.id === id)
+
   const [gif, setGif] = useState(gifFromCache)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
   useEffect(function () {
     if (!gif) {
+      setIsLoading(true)
+
       // llamar al servicio si no tenemos gif (recarga pagina detalles)
       getSingleGif({id})
-        .then(gif => setGif(gif))
+        .then(gif => {
+          setGif(gif)
+          setIsLoading(false)
+          setIsError(false)
+
+        }).catch(error => {
+          setIsError(true)
+        })
     }
   }, [gif, id])
 
-  return gif
+  return {gif, isLoading, isError}
 }
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import useSingleGif from 'hooks/useSingleGif';
+import Spinner from 'components/Spinner';
+import { Redirect } from 'wouter';
+
 
 export default function Detail({params}) {  
-  const gif = useSingleGif({id: params.id})
-  if (!gif) return null
+  const { gif, isLoading, isError } = useSingleGif({id: params.id})
+  
+  if ( isError ) return <Redirect to='/404' />
+  if ( isLoading ) return <Spinner />  
+  if ( !gif ) return null
 
   return (
     <React.Fragment>
