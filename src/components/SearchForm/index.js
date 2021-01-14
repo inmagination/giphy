4e49bae5f5ebc2a4b06d1bdfd4fb7021839d084b
Inma <ireyes@paradigmadigital.com>
2021-01-14 12:37:47 +0100

    buscador reset y estilos + reset button para formulario + arreglo dispatch rating + estilos basicos form

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,10 +1,18 @@
 import React, { useReducer } from 'react'
 import { useLocation } from "wouter";
+import 'components/SearchForm/styles.scss'
+
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 const ACTIONS = {
   UPDATE_KEYWORD: 'update_keyword',
-  UPDATE_RATING: 'update_rating'
+  UPDATE_RATING: 'update_rating',
+  RESET: 'reset'
+}
+const INITIAL_STATE = {
+  keyword: '',
+  rating: 'g', 
+  times: 0
 }
 
 const reducer = (state, action) => {
@@ -21,13 +29,20 @@ const reducer = (state, action) => {
         ...state,
         rating: action.payload
       }
+
+    case ACTIONS.RESET:
+      return {
+        keyword: INITIAL_STATE.keyword,
+        rating: INITIAL_STATE.rating, 
+        times: INITIAL_STATE.times
+      }
   
     default:
       return state
   }
 }
 
-function SearchForm ( { initialKeyword = '', initialRating = 'g' } ) {
+function SearchForm ( { initialKeyword = INITIAL_STATE.keyword, initialRating = INITIAL_STATE.rating } ) {
 
   const [state, dispatch] = useReducer(reducer, {
     keyword: decodeURIComponent(initialKeyword),
@@ -45,27 +60,38 @@ function SearchForm ( { initialKeyword = '', initialRating = 'g' } ) {
   }
 
   const handleChangeRating = event => {
-    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: event.target.value })
+    dispatch({ type: ACTIONS.UPDATE_RATING, payload: event.target.value })
   }
 
   const handleChange = event => {
-    dispatch({ type: ACTIONS.UPDATE_RATING, payload: event.target.value })
+    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: event.target.value })
   }  
 
-  return(    
-    <form onSubmit={handleSubmit}>      
-      <input 
-        type='text' 
-        value={keyword} 
-        onChange={handleChange} 
-        placeholder='Search a gif...'/>
+  const handleReset = event => {
+    event.preventDefault()
+    dispatch({ type: ACTIONS.RESET })
+  }
 
-      <select value={rating} onChange={handleChangeRating}>
-        {RATINGS.map(rating => <option key={rating}>{rating}</option>)}
-      </select>
+  return(     
+    <form onSubmit={handleSubmit} className='SearchForm'>   
+      <div className='SearchForm__data'>
+        <input 
+          type='text' 
+          value={keyword} 
+          onChange={handleChange} 
+          placeholder='Search a gif...'/>
 
-      <button>Buscar</button>
-      <small>{times}</small>
+        <select value={rating} onChange={handleChangeRating}>
+          {RATINGS.map(rating => <option key={rating}>{rating}</option>)}
+        </select>
+
+        <small>{times}</small>  
+      </div>   
+      
+      <div className='SearchForm__buttons'>
+        <button onClick={handleReset} className='SearchForm__reset'>Reset</button> 
+        <button>Buscar</button>         
+      </div>              
     </form>
   )
 }
