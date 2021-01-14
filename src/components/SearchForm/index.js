533f8e3ed909332eb0c8115314cd59ba6892a8bb
Inma <ireyes@paradigmadigital.com>
2021-01-14 11:19:09 +0100

    usar useReducer en buscador para actualizar estado keywords y times

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import { useLocation } from "wouter";
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
+const reducer = (state, param) => {
+  return {
+    ...state,
+    keyword: param,
+    times: state.times + 1
+  }
+}
+
 function SearchForm ( { initialKeyword = '', initialRating = 'g' } ) {
-  const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword))
   const [rating, setRating] = useState(initialRating)
-  const [, setLocation] = useLocation()
+
+  const [state, dispatch] = useReducer(reducer, {
+    keyword: decodeURIComponent(initialKeyword),
+    times: 0
+  })
+
+  const { keyword, times } = state
+
+  const [ _, setLocation] = useLocation()
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -18,8 +33,12 @@ function SearchForm ( { initialKeyword = '', initialRating = 'g' } ) {
   }
 
   const handleChange = event => {
-    setKeyword(event.target.value)
+    updateKeyword(event.target.value)
   }   
+
+  const updateKeyword = (keyword) => {
+    dispatch(keyword)
+  }
 
   return(    
     <form onSubmit={handleSubmit}>      
@@ -34,6 +53,7 @@ function SearchForm ( { initialKeyword = '', initialRating = 'g' } ) {
       </select>
 
       <button>Buscar</button>
+      <small>{times}</small>
     </form>
   )
 }
